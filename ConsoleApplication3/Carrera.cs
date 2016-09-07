ee61c2b17f208ef6affc82091799cc3e3eb2c286
laloo96 <ohmycaio@gmail.com>
2016-09-07 13:39:50 -0300

    Race sin listas.

---
+++ ConsoleApplication3/Carrera.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseTres
+{
+    public class Carrera
+    {
+        public Auto auto1;
+        public Auto auto2;
+        public Auto auto3;
+        public Auto auto4;
+        public Auto auto5;
+        public Auto auto6;
+        private static Random rnd;
+
+        public Carrera()
+        {
+            this.auto1 = new Auto();
+            this.auto2 = new Auto();
+            this.auto3 = new Auto();
+            this.auto4 = new Auto();
+            this.auto5 = new Auto();
+            this.auto6 = new Auto();
+            rnd = new Random();
+        }
+
+        public void CorrerCarreraTime(Tiempo time)
+        {
+            for (int i = 0; i < (int) time; i++)
+            {
+                this.auto1.Agregar((Kilometros)rnd.Next(10, 100), (int)time);
+                this.auto2.Agregar((Kilometros)rnd.Next(10, 100), (int)time);
+                this.auto3.Agregar((Kilometros)rnd.Next(10, 100), (int)time);
+                this.auto4.Agregar((Kilometros)rnd.Next(10, 100), (int)time);
+                this.auto5.Agregar((Kilometros)rnd.Next(10, 100), (int)time);
+                this.auto6.Agregar((Kilometros)rnd.Next(10, 100), (int)time);
+            }
+        }
+
+        public void CorrerCarrera(Kilometros kilometros)
+        {
+            for (int i = 0; i < (int) kilometros; i++)
+            {
+                this.auto1.Agregar((int)kilometros, (Tiempo)rnd.Next(10, 100));
+                this.auto2.Agregar((int)kilometros, (Tiempo)rnd.Next(10, 100));
+                this.auto3.Agregar((int)kilometros, (Tiempo)rnd.Next(10, 100));
+                this.auto4.Agregar((int)kilometros, (Tiempo)rnd.Next(10, 100));
+                this.auto5.Agregar((int)kilometros, (Tiempo)rnd.Next(10, 100));
+                this.auto6.Agregar((int)kilometros, (Tiempo)rnd.Next(10, 100));
+            }
+        }
+
+        public void MostrarCarrera()
+        {
+            this.auto1.MostarAuto();
+            this.auto2.MostarAuto();
+            this.auto3.MostarAuto();
+            this.auto4.MostarAuto();
+            this.auto5.MostarAuto();
+            this.auto6.MostarAuto();
+        }
+
+    }
+
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseTres
+{
+    class Program
+    {
+        static void Main()
+        {           
+            Carrera carrrera;
+            carrrera = new Carrera();
+            carrrera.CorrerCarreraTime(10);
+            carrrera.MostrarCarrera();
+
+            carrrera.CorrerCarrera(100);
+            carrrera.MostrarCarrera();
+
+            /*Rueda ggg = new Rueda("NIKE",40);
+
+            ggg.MostrarRueda();*/
+            /*
+            Kilometros tiempito;
+            tiempito = 90; //castea implicitamente a Tiempo y crea instancia de clase privada.
+            tiempito.MostrarKilom();
+            /*
+            Efabricante fabricante;
+
+            fabricante = Efabricante.Honda;
+            
+            Console.WriteLine(fabricante);
+            fabricante = (Efabricante) 1; // Casteo 1 que seria el 2 fabricante del enumerado ya que es el tipo de dato.
+
+
+           Console.WriteLine(fabricante);
+            
+            Auto nuevoAuto1 = new Auto();
+            Auto nuevoAuto2 = new Auto();
+            Auto nuevoAuto3 = new Auto();*/
+            
+            Console.ReadKey();
+        }
+    }
+}
